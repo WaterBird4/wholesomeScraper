1913231d6a4ca97637c6a5eee8ec707df79b2e45
Cristyle P Garrard <102877131+WaterBird4@users.noreply.github.com>
2023-03-20 14:30:49 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+# dash app to display wholesome data
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+
+# Do the things
+
+#
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
