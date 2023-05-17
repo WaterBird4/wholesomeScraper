f345025e72e429038f7ef7f7a6e1cc63c8ac88ac
Cristyle P Garrard <102877131+WaterBird4@users.noreply.github.com>
2023-05-17 14:53:16 -0600

    Update wholesomeWebscraper.py

--- wholesomeWebscraper.py
+++ wholesomeWebscraper.py
@@ -26,54 +26,53 @@ assert len(products) > 0
 product_dict = {}
 count = 0
 for val in products:
-	new_url = val.find_element(By.TAG_NAME, 'a').get_attribute('href')
-	#product_name = new_url.rsplit('/')[-1]
-	#temp_data = [product_name, new_url]
-	product_dict[count] = new_url
-	count += 1
+        new_url = val.find_element(By.TAG_NAME, 'a').get_attribute('href')
+        #product_name = new_url.rsplit('/')[-1]
+        #temp_data = [product_name, new_url]
+        product_dict[count] = new_url
+        count += 1
 # CHECK ALL PRODUCTS HAVE URLS
 # assert len(product_dict.values()) == len(products)
 # DO WE WANT IT TO FAIL IF ONE PRODUCT DOESN'T HAVE A URL OR WORK SO LONG AS WE HAVE AT LEAST 1 URL?
 assert len(product_dict.values()) > 0
 
 def build_data_table(driver):
-	data_table = driver.find_element(By.CLASS_NAME, "pdpAttributesTable")
-	rows = data_table.find_elements(By.TAG_NAME, "tr")
-	# CHECK YOU HAVE DATA TO BUILD THE DICTIONARY WITH
-	assert len(rows) > 0
-	data_dict = {}
-	for val in range(0, len(rows)):
-		data_key = rows[val].find_element(By.TAG_NAME, "th").text
-		data_value = rows[val].find_element(By.TAG_NAME, "td").text
-		data_dict[data_key] = data_value
-	return data_dict
+        data_table = driver.find_element(By.CLASS_NAME, "pdpAttributesTable")
+        rows = data_table.find_elements(By.TAG_NAME, "tr")
+        # CHECK YOU HAVE DATA TO BUILD THE DICTIONARY WITH
+        assert len(rows) > 0
+        data_dict = {}
+        for val in range(0, len(rows)):
+                data_key = rows[val].find_element(By.TAG_NAME, "th").text
+                data_value = rows[val].find_element(By.TAG_NAME, "td").text
+                data_dict[data_key] = data_value
+        return data_dict
 
 # RETRIEVE DATA FOR EACH PRODUCT AND BUILD DICTIONARY
 product_data_dict = {}
 for val in product_dict.keys():
-	goto_page = product_dict.get(val)
-	driver.get(goto_page)
-	wait = WebDriverWait(driver, 5)
-	data_dict = build_data_table(driver)
-	product_data_dict[val] = [goto_page, data_dict]
-	wait = WebDriverWait(driver, 5)
+        goto_page = product_dict.get(val)
+        driver.get(goto_page)
+        wait = WebDriverWait(driver, 5)
+        data_dict = build_data_table(driver)
+        product_data_dict[val] = [goto_page, data_dict]
+        wait = WebDriverWait(driver, 5)
 
-# ———————— pivot data
+# ———————— PIVOT DATA
 import pandas as pd
-
 
 df = pd.DataFrame.from_dict(product_data_dict, orient='index')
 all_data = pd.DataFrame()
 for val in range(0, len(product_data_dict.keys())):
-	temp = pd.DataFrame.from_dict(df.loc[val][1], orient="index").transpose()
-	temp["product_name"] = product_data_dict.get(val)[0].rsplit('/')[-1]
-	all_data = all_data.append(temp, ignore_index=True)
+        temp = pd.DataFrame.from_dict(df.loc[val][1], orient="index").transpose()
+        temp["product_name"] = product_data_dict.get(val)[0].rsplit('/')[-1]
+        all_data = all_data.append(temp, ignore_index=True)
 
 all_data.reset_index(inplace=True, drop=True)
 
-# ———— save data to a file
+# ———— SAVE DATA TO FILE
 from datetime import datetime
 
-filename = "product_data_" + datetime.today().strftime("%m_%d_%Y")+ ".txt"
+filename = "product_data_" + datetime.today().strftime("%m_%d_%Y_%H%M")+ ".txt"
 with open(filename, "w") as f:
-	all_data.to_csv(f, index=False)
+        all_data.to_csv(f, index=False)
